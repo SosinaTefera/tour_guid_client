4d66cdb6b41cb0f197bbd3e51c70ceaf2e62c9d8
SosinaTefera <diyanatefera123@gmail.com>
2025-03-05 10:36:42 +0300

    Ui change on dashboard and sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,23 +1,116 @@
-// src/components/Sidebar.js
-import React from "react";
-import { Drawer, List, ListItem, ListItemText } from "@mui/material";
-import { Link } from "react-router-dom";
-
+import { AccountBox, AddCircle, BookmarkAdded, BrowseGallery, CalendarViewMonth, Dashboard, Payment, Tour } from "@mui/icons-material";
+import { Box, Divider, List, ListItem, ListItemIcon, ListItemText, Typography } from "@mui/material";
+import { Link, useLocation } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 const Sidebar = () => {
+  const location = useLocation();
+  const {user}=useAuth()
+  const menuItems =user.role ==='admin'? [
+    { text: "Analytics", path: "/dashboard", icon: <Dashboard /> },
+    { text: "Tours", path: "/dashboard/tours_admin", icon: <Tour /> },
+    { text: "Create Tour", path: "/dashboard/create-tour", icon: <AddCircle /> },
+
+  ]:[
+    { text: "Tours", path: "/dashboard/tours", icon: <Tour /> },
+
+    { text: "Account", path: "/dashboard/account", icon: <AccountBox /> },
+  ];
+
+  const activePath = location.pathname.startsWith("/dashboard/tours_admin")
+  ? "/dashboard/tours_admin"
+  : location.pathname.startsWith("/dashboard/tours")
+  ? "/dashboard/tours"
+  : location.pathname === "/dashboard"
+  ? menuItems[0].path
+  : location.pathname;
   return (
-    <Drawer variant="permanent" sx={{ width: 240, flexShrink: 0 }}>
-      <List>
-        <ListItem button component={Link} to="/dashboard/tours">
-          <ListItemText primary="Tours" />
+    <Box
+    sx={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
+      height: "100vh",
+      width:'100%'
+      
+  }}
+>
+  <Box
+    sx={{
+      borderRight: "1px solid #e0e0e0",
+      backgroundImage: 'url("https://images.unsplash.com/photo-1448375240586-882707db888b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      height:'100vh',
+      width:'100%'
+  }}
+    
+  >
+    <Box sx={{
+      width:'100%',
+      mt:'30px',
+      display:'flex',
+      gap:'10px'
+    }}>
+      <Box
+      sx={{
+        borderRight: "1px solid #e0e0e0",
+        backgroundImage: 'url("https://images.unsplash.com/photo-1682687220801-eef408f95d71?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height:'70px',
+        width:'70px',
+        borderRadius:'50%',
+    }}
+      >
+
+      </Box>
+      <h1 style={{color:'white'}}>TOUR WITH US</h1>
+    </Box>
+    <Divider  sx={{width:'100%',height:'1px',bgcolor:'white'}} />
+   <Box sx={{
+    mt:'20px',
+    display:'flex',
+    flexDirection:'column',
+    gap:'10px',
+    width:'100%',
+    justifyContent:'center',
+    alignItems:'center',
+   }}>
+    
+   {menuItems.map((item) => (
+        <ListItem
+          button
+          key={item?.text}
+          component={Link}
+          to={item.path}
+          sx={{
+            backgroundColor:
+              activePath === item.path ? "#1976d2" : "transparent",
+            color: activePath === item.path ? "#fff" : "whitesmoke",
+            "&:hover": {
+              backgroundColor: "#eeeeee",
+              color: "#333",
+            },
+            width:'80%',
+            borderRadius:'10px',
+          }}
+        >
+          <ListItemIcon sx={{ color: activePath === item.path ? "#fff" : "#fff" ,
+             "&:hover": {
+              color: "black",
+            },
+          }}>
+            {item.icon}
+          </ListItemIcon>
+          <ListItemText primary={item.text} />
         </ListItem>
-        <ListItem button component={Link} to="/dashboard/create-tour">
-          <ListItemText primary="Create Tour" />
-        </ListItem>
-        <ListItem button component={Link} to="/dashboard/analytics">
-          <ListItemText primary="Analytics" />
-        </ListItem>
-      </List>
-    </Drawer>
+      ))}
+    </Box>
+
+  </Box>
+
+
+    </Box>
   );
 };
 
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,14 +1,35 @@
-import React from "react";
-import Sidebar from "../../components/Sidebar";
-import { Outlet } from "react-router-dom";
-import { Box } from "@mui/material";
+import React, {useEffect} from 'react';
+import Sidebar from '../../components/Sidebar';
+import {NavLink, Outlet, useNavigate} from 'react-router-dom';
+import {Box} from '@mui/material';
+import ResponsiveAppBar from './Home';
+import { useAuth } from "../../context/AuthContext";
 
 const Dashboard = () => {
+  const navigate = useNavigate ();
+  const {user} = useAuth();
+
+  useEffect (
+    () => {
+      if (!user) {
+        console.log ({user});
+        navigate ('/login');
+      }
+    },
+    [user, navigate]
+  );
   return (
-    <Box sx={{ display: "flex" }}>
-      <Sidebar />
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <Outlet /> 
+     <Box sx={{ alignItems: 'center', display: 'flex', width: '100%', height: '100vh', overflow: 'hidden' }}>
+      <Box sx={{ width: '20%', display: 'flex', overflow: 'hidden' }}>
+        <Sidebar />
+      </Box>
+      <Box sx={{ width: '80%', height: '100vh', overflow: 'hidden' }}>
+        <Box sx={{ width: '100%', height: '70px', display: 'flex' }}>
+          <ResponsiveAppBar />
+        </Box>
+        <Box sx={{ height: 'calc(100vh - 70px)', overflow: 'hidden', display: 'flex' }}>
+          <Outlet />
+        </Box>
       </Box>
     </Box>
   );
